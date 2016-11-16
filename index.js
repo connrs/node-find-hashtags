1daa5040bb6b44913fe7c245c35e1b164ac1c88c
Allxie <allxie@way2b1.com>
2016-11-16 14:40:23 -0800

    Add feature that can detect @atStrings as well as the existing @hashtagStrings

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var XRegExp = require('xregexp').XRegExp;
 var hashtagExp = XRegExp('#[\\p{L}\\d]+', 'i');
+var atExp = XRegExp('@[\\p{L}\\d]+', 'i');
 
 function FindHashtags() {
   this._nextMatchPosition = 0;
@@ -71,4 +72,78 @@ function findHashtags(content) {
   return tags.getHashtags();
 }
 
-module.exports = findHashtags;
+//@@@@@@@@@@@@@@@
+function FindAt() {
+  this._nextMatchPosition = 0;
+  this._matches = [];
+  this._currentMatch = null;
+  this._currentMatchText = null;
+  this._content = null;
+}
+
+FindAt.prototype.setContent = function (content) {
+  this._content = content;
+};
+
+FindAt.prototype.getHashtags = function () {
+  this._loopMatches();
+  return this._getMatches();
+};
+
+FindAt.prototype._loopMatches = function () {
+  this._findNextMatch();
+
+  while (this._foundMatch()) {
+    this._setCurrentMatchText();
+
+    if (this._currentMatchIsUnique()) {
+      this._addCurrentResultToMatches();
+    }
+
+    this._incrementNextMatchPosition();
+    this._findNextMatch();
+  }
+};
+
+FindAt.prototype._findNextMatch = function () {
+  this._currentMatch = XRegExp.exec(this._content, atExp, this._nextMatchPosition);
+};
+
+FindAt.prototype._foundMatch = function () {
+  return this._currentMatch === null ? false : true;
+};
+
+FindAt.prototype._setCurrentMatchText = function () {
+  this._currentMatchText = this._formatMatchText(this._currentMatch[0]);
+};
+
+FindAt.prototype._formatMatchText = function (match) {
+  return match.substring(1).toLowerCase();
+};
+
+FindAt.prototype._currentMatchIsUnique = function () {
+  return this._matches.indexOf(this._currentMatchText) === -1;
+};
+
+FindAt.prototype._addCurrentResultToMatches = function () {
+  this._matches.push(this._currentMatchText);
+};
+
+FindAt.prototype._incrementNextMatchPosition = function () {
+  this._nextMatchPosition = this._currentMatch.index + this._currentMatch[0].length;
+};
+
+FindAt.prototype._getMatches = function () {
+  return this._matches.length === 0 ? [] : this._matches;
+};
+
+function findAt(content) {
+  var tags = new FindAt();
+  tags.setContent(content);
+  return tags.getHashtags();
+}
+
+module.exports = {
+  findHashtags:findHashtags,
+  findAt:findAt
+};
